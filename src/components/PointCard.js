bc7e30bf3feaad6b77f9556ef1f69d8aa64e8a07
James <james.griffith9519@gmail.com>
2022-09-28 00:10:00 -0500

    clean up console errors and add missing total points text

--- src/components/PointCard.js
+++ src/components/PointCard.js
@@ -3,13 +3,23 @@ import { getTotalPoints } from "../utils";
 import "../styles/PointCard.css";
 
 export default function PointCard({ pointsData }) {
+  const customerTotalPoints = pointsData?.transactions
+    ?.map((transactionRecord) => {
+      return transactionRecord.map((record) => record.transactionPoints);
+    })
+    .flat(1)
+    .reduce((a, b) => a + b, 0);
+
   return (
     <div className="pointCardContainer">
-      <h1 className="pointCardHeading">{pointsData?.customerName}</h1>
+      <div className="pointCardHeading">
+        <h1>{pointsData?.customerName}</h1>
+        <h2>Total Points Earned: {customerTotalPoints}</h2>
+      </div>
       <div className="pointCardBody">
-        {pointsData?.transactions?.map((transaction) => {
+        {pointsData?.transactions?.map((transaction, index) => {
           return (
-            <div className="transactionContainer">
+            <div className="transactionContainer" key={`${index}`}>
               <h3>{transaction[0].purchaseMonth} Transactions</h3>
               {transaction.map((transactionRecord) => {
                 return (
